a5ed54bf3a04ecd50931ad0f0f063f1ed1e47be5
dilanvoosten <dilanoosten@gmail.com>
2024-07-02 13:01:41 +0200

    feat(dices): #5 Made the database schema for saving the scores users and gametypes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Personal Projects WebApp</h1>
+<h3>Tools</h3>
+<p>This WebApp is made in SvelteKit. Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    <br>
+    This project makes use of </p>
 
